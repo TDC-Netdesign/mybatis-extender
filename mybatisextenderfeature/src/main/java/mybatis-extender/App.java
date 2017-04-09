94364d849ef8f00e0025ee5aa0aa42dd05b2e1da
Nino Martinez Wael <nino.martinez.wael@gmail.com>
2017-04-09 20:48:58 +0200

    building structure

---
+++ mybatisextenderfeature/src/main/java/mybatis-extender/App.java
@@ -0,0 +1,13 @@
+package mybatis-extender;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ mybatisextenderruntime/src/main/java/org/ops4j/pax/App.java
@@ -0,0 +1,13 @@
+package org.ops4j.pax;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
