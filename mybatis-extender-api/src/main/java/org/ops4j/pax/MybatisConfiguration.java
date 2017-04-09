22c20e5447adc6bd4019606709db8715de56374d
Nino Martinez Wael <nino.martinez.wael@gmail.com>
2017-04-09 22:32:38 +0200

    no message

---
+++ mybatis-extender-api/src/main/java/org/ops4j/pax/MybatisConfiguration.java
@@ -0,0 +1,40 @@
+package org.ops4j.pax;
+
+import java.util.List;
+
+/**
+ * Created by nino on 09-04-2017.
+ */
+public interface MybatisConfiguration {
+
+    /**
+     *
+     * @return the list of mappers for this configuration
+     */
+    public List<Class> getMappers();
+
+    /**
+     *
+     * @return a valid jdbc url string
+     */
+    public String getJDBCUrl();
+
+    /**
+     *
+     * @return get password for JDBC
+     */
+    public String getPassword();
+
+    /**
+     *
+     * @return user for JDBC
+     */
+    public String getUser();
+
+    /**
+     * driver used for jdbc connection, will be loaded in to the bundles context.
+     * @return
+     */
+    public Class getDriver();
+}
+
